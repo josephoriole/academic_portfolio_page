8a782cb4246910991c61ee87b9aae6723a2ff9e6
Joseph Oriole <10joscojunior@gmail.com>
2026-03-04 21:39:23 +0100

    update

--- learning.js
+++ learning.js
@@ -6,3 +6,49 @@ if (football === '1:00pm') {
 }else { 
     console.log('it false sir');
 }
+
+let d = 5;
+let b=10;
+let c=5<10;
+if (c === false){ 
+    console.log('the equation is correct');
+}else{
+   console.log('the equation is wrong'); 
+}
+ 
+let age = 18;
+let valid = 25;
+let restriction = 25<18;
+let permission = 25>18;
+if (valid >18){
+    console.log('free access'); 
+} else if (restriction <25) {
+     console.log( 'no access to the app');
+} else {
+    console.log('no access');
+}
+
+ let x = 67;
+ let y = x + 4;
+ console.log(y); 
+
+ let a = 2
+b=1;
+console.log(a>b); 
+
+const name = "joseph"; 
+console.log("My name is " + name);
+
+
+let fname = "anslem", lname = "miracle";
+let fullname = fname +" "+lname;
+console.log("Hello My full name is:" + fullname); 
+
+console.log('A' + '  ' + 'string') 
+
+
+let sname = "jennifer"
+let Lname = "angela"
+fname = sname +" "+ Lname;
+console.log('my full name is' + fname);
+  
